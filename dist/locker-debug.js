f7e48d0ea5538850274c4cfcfa6adc1ae26d9c77
mssk <mssk1@126.com>
2014-09-23 10:22:37 +0800

    解决ie8及以下浏览器数组不支持indexOf的问题

--- dist/locker-debug.js
+++ dist/locker-debug.js
@@ -1,23 +1,24 @@
-define("crossjs/locker/0.1.0/locker-debug", [], function(require, exports, module) {
+define("pandora/locker/1.0.0/locker-debug", [], function(require, exports, module) {
     /**
- * 对象储物柜
+ * 储物柜
  * @module Locker
  */
     "use strict";
-    var prefix = "CROSSJS-LOCKER";
+    var prefix = "LKR-";
     /**
- * 对象存取储物柜
+ * 储物柜
  * @class Locker
  * @constructor
  */
     var Locker = function() {
-        this.stacks = {};
+        this.empty();
     };
     Locker.prototype = {
         /**
    * 从储物柜中取得指定对象，参数key未指定则返回全部
    * @param {mix} [key] 对象key
    * @method get
+   * @return {mixed} 返回key对应的对象，key未指定则返回全部，有指定但未找到则返回null
    */
         get: function(key) {
             if (typeof key !== "undefined") {
@@ -32,11 +33,19 @@ define("crossjs/locker/0.1.0/locker-debug", [], function(require, exports, modul
         /**
    * 将对象存入储物柜
    * @param {string} key 对象key
-   * @param {object} object 对象
+   * @param {object} obj 对象
+   * @param {number} [index] 插入的位置
    * @method set
    */
-        set: function(key, obj) {
+        set: function(key, obj, index) {
             this.stacks[prefix + key] = obj;
+            if (this.indexOfArray(this.keys, key) === -1) {
+                if (typeof index !== "undefined") {
+                    this.keys.splice(index, 0, key);
+                } else {
+                    this.keys.push(key);
+                }
+            }
         },
         /**
    * 从储物柜中删除指定对象
@@ -46,6 +55,7 @@ define("crossjs/locker/0.1.0/locker-debug", [], function(require, exports, modul
         remove: function(key) {
             if (key !== "") {
                 delete this.stacks[prefix + key];
+                this.keys.splice(this.indexOfArray(this.keys, key), 1);
             }
         },
         /**
@@ -54,6 +64,49 @@ define("crossjs/locker/0.1.0/locker-debug", [], function(require, exports, modul
    */
         empty: function() {
             this.stacks = {};
+            this.keys = [];
+        },
+        /**
+   * 物品数量
+   * @method length
+   * @return {Number} 返回物品数量
+   */
+        length: function() {
+            return this.keys.length;
+        },
+        /**
+   * 第一个物品
+   * @method first
+   * @return {mixed} 返回第一个物品
+   */
+        first: function() {
+            return this.get(this.keys[0]);
+        },
+        /**
+   * 最后一个物品
+   * @method last
+   * @return {mixed} 返回最后一个物品
+   */
+        last: function() {
+            return this.get(this.keys[this.length() - 1]);
+        },
+        /**
+   * 兼容IE8及以下不支持Array的indexOf方法
+   * @param  {Array} array 数组
+   * @param  {String|Number} value 值
+   * @return {Number} value在array中的下标，如果没找到返回-1
+   */
+        indexOfArray: function(array, value) {
+            var index = -1, length = array.length;
+            if (array.indexOf) {
+                return array.indexOf(value);
+            }
+            while (length--) {
+                if (array[length] === value) {
+                    return length;
+                }
+            }
+            return index;
         }
     };
     return Locker;
--- src/locker.js
+++ src/locker.js
@@ -47,7 +47,7 @@ Locker.prototype = {
   set: function (key, obj, index) {
     this.stacks[prefix + key] = obj;
 
-    if (this.keys.indexOf(key) === -1) {
+    if (this.indexOfArray(this.keys, key) === -1) {
       if (typeof index !== 'undefined') {
        this.keys.splice(index, 0, key);
       } else {
@@ -65,7 +65,7 @@ Locker.prototype = {
     if (key !== '') {
       delete this.stacks[prefix + key];
 
-      this.keys.splice(this.keys.indexOf(key), 1);
+      this.keys.splice(this.indexOfArray(this.keys, key), 1);
     }
   },
 
@@ -103,8 +103,27 @@ Locker.prototype = {
    */
   last: function () {
     return this.get(this.keys[this.length() - 1]);
-  }
+  },
 
+  /**
+   * 兼容IE8及以下不支持Array的indexOf方法
+   * @param  {Array} array 数组
+   * @param  {String|Number} value 值
+   * @return {Number} value在array中的下标，如果没找到返回-1
+   */
+  indexOfArray : function(array, value){
+    var index = -1,
+        length = array.length;
+    if(array.indexOf){
+      return array.indexOf(value);
+    }
+    while(length--){
+      if(array[length] === value){
+        return length;
+      }
+    }
+    return index;
+  }
 };
 
 return Locker;
